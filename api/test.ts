4cd87c3e85385f0c1517c8e3580074cbedab25e8
Minh Dinh <macotontai123@gmail.com>
2025-05-17 15:18:53 +0700

    chore(test): test stuff for get/set data

--- api/test.ts
+++ api/test.ts
@@ -3,15 +3,23 @@ interface User {
   name: string;
 }
 
-const testUser: User = {
+// const testUser: User = {
+//   userId: "1",
+//   name: "Tien da den",
+// };
+
+(global as any).testUser = {
   userId: "1",
-  name: "Tien da den",
+  name: "tien da den",
 };
 
-export async function getUser(userId: string) {
-  return testUser;
+export async function getUser(userId: string): Promise<User> {
+  return global.testUser;
 }
 
-export async function updateUser(userId: string, newName: string) {
-  testUser.name = newName;
+export async function updateUser(
+  userId: string,
+  newName: string,
+): Promise<void> {
+  global.testUser.name = newName;
 }
