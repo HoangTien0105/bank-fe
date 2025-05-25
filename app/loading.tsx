786100cf92e1209cd7892268b26033d41f557c25
Minh Dinh <macotontai123@gmail.com>
2025-05-25 11:32:18 +0700

    feat(loading): update loading section

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,14 @@
+import { Box, Spinner, Text, VStack } from "@chakra-ui/react";
+
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="flex min-h-screen justify-center items-center">
+      <VStack colorPalette="blue">
+        <Spinner color="colorPalette.600" />
+        <Text color="colorPalette.600">Loading...</Text>
+      </VStack>{" "}
+    </Box>
+  );
 };
 
 export default Loading;
